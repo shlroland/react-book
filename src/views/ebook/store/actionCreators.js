ac11d5032d193787552e72df890fc1bf4910bc03
shlroland <shlroland1995@gmail.com>
2020-04-28 21:38:05 +0800

    补充完整redux

--- src/views/ebook/store/actionCreators.js
+++ src/views/ebook/store/actionCreators.js
@@ -1,17 +1,86 @@
-import * as actionTypes from './constants';
+import * as actionTypes from './constants'
 // import { fromJS } from 'immutable';
 
 export const changeFileName = (data) => ({
-    type:actionTypes.CHANGE_FILENAME,
-    data
+  type: actionTypes.CHANGE_FILENAME,
+  data,
 })
 
 export const changeMenuVisible = (data) => ({
-    type:actionTypes.CHANGE_MENUVISIBLE,
-    data
+  type: actionTypes.CHANGE_MENU_VISIBLE,
+  data,
 })
 
 export const changeSettingVisible = (data) => ({
-    type:actionTypes.CHANGE_SETTINGVISIBLE,
-    data
+  type: actionTypes.CHANGE_SETTING_VISIBLE,
+  data,
+})
+
+export const changeDefaultFontSize = (data) => ({
+  type: actionTypes.CHANGE_DEFAULT_FONT_SIZE,
+  data,
+})
+export const changeDefaultFontFamily = (data) => ({
+  type: actionTypes.CHANGE_DEFAULT_FONT_FAMILY,
+  data,
+})
+export const changeFontFamilyVisible = (data) => ({
+  type: actionTypes.CHANGE_FONT_FAMILY_VISIBLE,
+  data,
+})
+export const changeDefaultTheme = (data) => ({
+  type: actionTypes.CHANGE_DEFAULT_THEME,
+  data,
+})
+export const changeBookAvailable = (data) => ({
+  type: actionTypes.CHANGE_BOOK_AVAILABLE,
+  data,
+})
+export const changeProgress = (data) => ({
+  type: actionTypes.CHANGE_PROGRESS,
+  data,
+})
+export const changeSection = (data) => ({
+  type: actionTypes.CHANGE_SECTION,
+  data,
+})
+export const changeIsPaginating = (data) => ({
+  type: actionTypes.CHANGE_IS_PAGINATING,
+  data,
+})
+export const changeCurrentBook = (data) => ({
+  type: actionTypes.CHANGE_CURRENT_BOOK,
+  data,
+})
+export const changeNavigation = (data) => ({
+  type: actionTypes.CHANGE_NAVIGATION,
+  data,
+})
+export const changeCover = (data) => ({
+  type: actionTypes.CHANGE_COVER,
+  data,
+})
+export const changeMetadata = (data) => ({
+  type: actionTypes.CHANGE_METADATA,
+  data,
+})
+export const changePaginate = (data) => ({
+  type: actionTypes.CHANGE_PAGINATE,
+  data,
+})
+export const changePagelist = (data) => ({
+  type: actionTypes.CHANGE_PAGELIST,
+  data,
+})
+export const changeOffsety = (data) => ({
+  type: actionTypes.CHANGE_OFFSETY,
+  data,
+})
+export const changeIsBookmark = (data) => ({
+  type: actionTypes.CHANGE_IS_BOOKMARK,
+  data,
+})
+export const changeSpeakingIconBottom = (data) => ({
+  type: actionTypes.CHANGE_SPEAKING_ICON_BOTTOM,
+  data,
 })
--- src/views/ebook/store/reducer.js
+++ src/views/ebook/store/reducer.js
@@ -5,16 +5,67 @@ const defaultState = fromJS({
   fileName: '',
   menuVisible: false,
   settingVisible: -1,
+  defaultFontSize: 16,
+  defaultFontFamily: 'Default',
+  fontFamilyVisible: false,
+  defaultTheme: 'Default',
+  bookAvailable: false,
+  progress: 0,
+  section: 0,
+  isPaginating: true,
+  currentBook: null,
+  navigation: null,
+  cover: null,
+  metadata: null,
+  paginate: '',
+  pagelist: null,
+  offsetY: 0,
+  isBookmark: null,
+  speakingIconBottom: 58
 })
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case actionTypes.CHANGE_FILENAME:
       return state.set('fileName', action.data)
-    case actionTypes.CHANGE_MENUVISIBLE:
+    case actionTypes.CHANGE_MENU_VISIBLE:
       return state.set('menuVisible', action.data)
-    case actionTypes.CHANGE_SETTINGVISIBLE:
+    case actionTypes.CHANGE_SETTING_VISIBLE:
       return state.set('settingVisible', action.data)
+    case actionTypes.CHANGE_DEFAULT_FONT_SIZE:
+      return state.set('defaultFontSize'.action.data)
+    case actionTypes.CHANGE_DEFAULT_FONT_FAMILY:
+      return state.set('defaultFontFamily'.action.data)
+    case actionTypes.CHANGE_FONT_FAMILY_VISIBLE:
+      return state.set('fontFamilyVisible'.action.data)
+    case actionTypes.CHANGE_DEFAULT_THEME:
+      return state.set('defaultTheme'.action.data)
+    case actionTypes.CHANGE_BOOK_AVAILABLE:
+      return state.set('bookAvailable'.action.data)
+    case actionTypes.CHANGE_PROGRESS:
+      return state.set('progress'.action.data)
+    case actionTypes.CHANGE_SECTION:
+      return state.set('section'.action.data)
+    case actionTypes.CHANGE_IS_PAGINATING:
+      return state.set('isPaginating'.action.data)
+    case actionTypes.CHANGE_CURRENT_BOOK:
+      return state.set('currentBook'.action.data)
+    case actionTypes.CHANGE_NAVIGATION:
+      return state.set('navigation'.action.data)
+    case actionTypes.CHANGE_COVER:
+      return state.set('cover'.action.data)
+    case actionTypes.CHANGE_METADATA:
+      return state.set('metadata'.action.data)
+    case actionTypes.CHANGE_PAGINATE:
+      return state.set('paginate'.action.data)
+    case actionTypes.CHANGE_PAGELIST:
+      return state.set('pagelist'.action.data)
+    case actionTypes.CHANGE_OFFSETY:
+      return state.set('offsetY'.action.data)
+    case actionTypes.CHANGE_IS_BOOKMARK:
+      return state.set('isBookmark'.action.data)
+    case actionTypes.CHANGE_SPEAKING_ICON_BOTTOM:
+      return state.set(''.action.data)
     default:
       return state
   }
